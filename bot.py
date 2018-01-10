116fed295dd84698a473954be096099d429ca987
Jenevra <sunwillshine96@outlook.com>
2018-01-10 21:36:31 +0300

    echo bot

---
+++ bot.py
@@ -0,0 +1,24 @@
+import config
+import telebot
+import os
+import socket
+import re
+from telebot import types
+
+print('hello')
+
+# create object bot
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+sock = socket.socket()
+sock.bind(('', int(os.environ.get('PORT', '5000'))))
+sock.listen(1)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+token = '486255901:AAEToNghL5GfbOrbNSD5_pBj-gQ2_zxQfrQ'
