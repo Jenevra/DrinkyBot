96b63736060d901b34b2dcd7d8e81b41db3c8e60
Jenevra <sunwillshine96@outlook.com>
2018-04-13 13:11:21 +0300

    check

---
+++ text.py
@@ -0,0 +1,27 @@
+start_message_unknown_user = 'Hi, I guess you\'re new.\n' +\
+                             "Now I'll explain you how to work with me.\n" + \
+                             'I want to offer you something to drink.\n' + \
+                             'Use /products to enter a product or list of products divided by ,\n'
+
+start_message_known_user = 'Hello, you really know what to do!'
+
+choose_product_message = 'Please enter one or more product.\n' + \
+                         'Divide them by , or by ENTER'
+
+help_message = 'Do you need help? Here some advice.\n'
+
+reset_message = "Goodbye, see you later :)"
+
+
+answer_hello = ["Hello, do you wanna start or already have started?\n"
+                "Press /products to enter your choice",
+                "Hi, wanna play? Press /products",
+                "Hello, do you need /help?",
+                "Hi, let's start"]
+
+answer_help = "Please, press /help to get an advice"
+
+nothing_to_find = ["I'm sorry my friend, but you have so special taste\n"
+                   "I guess I can recommend you water, it's the best ",
+                   "Oh sorry, may be you can enter something different?",
+                   "I'm tired, another time, sorry"]
